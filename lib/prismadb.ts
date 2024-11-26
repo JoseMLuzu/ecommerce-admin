af32156b6fc877c8a9d85a45b177ab715a440b09
JoseMLuzu <josemanuelluzuriaga@gmail.com>
2024-11-25 23:52:12 -0500

    Add some components for the admin and connect database with neon

---
+++ lib/prismadb.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+// Declaración global para evitar múltiples instancias en desarrollo
+declare global {
+  // Solo se aplica en el entorno Node.js
+  var prisma: PrismaClient | undefined;
+}
+
+const prismadb = globalThis.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prismadb;
+}
+
+export default prismadb;
